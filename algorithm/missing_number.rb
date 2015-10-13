e6cc0db2719ad4b03185000ba8f72fb1948bd609
Sarah Kwak <sarahkwak@hotmail.com>
2015-10-12 21:46:47 -0700

    update missing num

--- algorithm/missing_number.rb
+++ algorithm/missing_number.rb
@@ -2,13 +2,12 @@ def missing_number(arr)
   i = 0
   while i < arr.length-1
     if arr[i] != arr[i+1]-1
-      arr[i]
-      arr[i+1]-1
-      return false
+      return arr[i]+1
     end
     i+=1
   end
-  return true
+  return 'nothing is missing'
 end
 
-p missing_number([1,2,3,4, 5, 6])
+p missing_number([1, 3, 4, 5, 6])    # => 2
+p missing_number([1, 2, 3, 4, 6])    # => 5
